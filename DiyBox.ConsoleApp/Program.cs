6f5c14320dd5389efaf23e6090cfd502f684f5d1
krzm <kmazanek@gmail.com>
2022-04-13 12:11:03 +0200

    Remove app

--- DiyBox.ConsoleApp/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using DIHelper;
-using DiyBox.ConsoleApp;
-using Unity;
-
-IBootstraper booter = new Bootstraper(
-	new UnityDependencySuite(
-		new UnityContainer().AddExtension(new Diagnostic())));
-booter.Boot(args);
