94e99e66c012ed2c05b92f14ac3c2276231ebbf2
krzm <kmazanek@gmail.com>
2022-04-19 11:47:32 +0200

    Add dependency set

--- DiyBox.CommandDotNet/SizeArg.cs
+++ DiyBox.CommandDotNet/SizeArg.cs
@@ -11,8 +11,8 @@ public class SizeArg
     [Operand(nameof(Length)), Required, Range(0.1, double.MaxValue, ErrorMessage = DimError)]
     public double Length { get; set; }
 
-    [Operand(nameof(Heigth)), Required, Range(0.1, double.MaxValue, ErrorMessage = DimError)]
-	public double Heigth { get; set; }
+    [Operand(nameof(Height)), Required, Range(0.1, double.MaxValue, ErrorMessage = DimError)]
+	public double Height { get; set; }
     
     [Operand(nameof(Depth)), Required, Range(0.1, double.MaxValue, ErrorMessage = DimError)]
 	public double Depth { get; set; }
--- DiyBox.Core/DependencySet/DiyBoxSet.cs
+++ DiyBox.Core/DependencySet/DiyBoxSet.cs
@@ -14,8 +14,16 @@ public class DiyBoxSet
     public override void Register()
     {
         base.Register();
-        Container.RegisterType<IArgsParser<Size3d>, DiyBoxParser>();
-        Container.RegisterType<IDiyBoxWizard, DiyBoxWizard>(nameof(DiyBoxWizard));
-        Container.RegisterType<IDiyBoxWizard, SheetWizard>(nameof(SheetWizard));
+        Container
+            .RegisterType<IArgsParser<Size3d>, DiyBoxParser>()
+            .RegisterType<IBox, Box>()
+            .RegisterType<ISheet, Sheet>()
+            .RegisterType<ISheetCalculator, SheetCalculator>()
+            .RegisterType<IWaste, Waste>()
+            .RegisterType<IBoxCalculator, BoxCalculator>()
+            .RegisterType<IDiyBoxWizard, SheetWizard>(
+                nameof(SheetWizard))
+            .RegisterType<IDiyBoxWizard, DiyBoxWizard>(
+                nameof(DiyBoxWizard));
     }
 }
