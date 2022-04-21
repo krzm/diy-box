0aaceff0a3f8908a975dfce06a0c3d55a26aa0e6
krzm <kmazanek@gmail.com>
2022-04-21 20:44:11 +0200

    Add folders to tests

--- DiyBox.Tests/TapeMarker/VerticalSideTapeMarkerTests.cs
+++ DiyBox.Tests/TapeMarker/VerticalSideTapeMarkerTests.cs
@@ -27,3 +27,28 @@ public class VerticalSideTapeMarkerTests
 		Assert.Equal(marker3, sut.GetMark("Fold2"));
 	}
 }
+
+public class BoxCalcTests
+    : TapeMarkerTests
+{
+	[Theory]
+	[InlineData(20, 30, 10, 40, 50)]
+	public void Convert_Vertical_Side_Points_To_Tape_Marks(
+		double length
+		, double heigth
+		, double marker1
+		, double marker2
+		, double marker3)
+	{
+		var mock = SetBoxCalcMock(
+            new BoxMockData(length, heigth, 0.1, 0.1)
+            , new WasteMockData());
+		var sut = new VerticalSideTapeMarker();
+
+		sut.Calculate(mock.Object);
+		
+		Assert.Equal(marker1, sut.GetMark("Fold1"));
+		Assert.Equal(marker2, sut.GetMark("Wall"));
+		Assert.Equal(marker3, sut.GetMark("Fold2"));
+	}
+}
